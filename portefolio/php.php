249322fb3f5a3bd17e0a44046f08fbd0e1ebb522
Valentin Choisnet <vchoisnet@sio.local>
2022-11-17 17:01:11 +0100

    maj

---
+++ portefolio/php.php
@@ -0,0 +1,40 @@
+<?php
+
+$content=file_get_contents('yaml.yaml');
+$yamlContent=yaml_parse($content);
+//echo '<pre>'.print_r($yamlContent,true).'</pre>';
+$competences=$yamlContent['compétences'];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<ul class='elm'>";
+    foreach($competences as $comp){
+        echo "<li>$comp</li>";
+    }
+    echo "</ul>";
+    ?>
+
+
+
+
+
+
+</body>
+</html>
+   
+
+    
+
+
+
+
