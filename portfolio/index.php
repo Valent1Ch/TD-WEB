ee2ed0d1f0ae6864a28dfb2eaee925a4071c8ba5
Valent1Ch <valentin.choisnet@sts-sio-caen.info>
2022-12-08 20:54:23 +0100

    maj
    bouton

--- portfolio/index.php
+++ portfolio/index.php
@@ -5,17 +5,25 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="" type="image/x-icon">
     <title>Document</title>
 </head>
 <body>
-   <nav>
+<br>   
+<nav>
     <ol>
-        <a href="index.php#accueil">Accueil</a> 
-        <a href="index.php#Apropos">A propos</a>
-        <a href="index.php#competences">Compétences</a>
-        <a href="index.php#experience">Expérience</a>
-        <a href="">Formation</a>
-        <a href="">Contact</a>
+
+
+
+
+    <a class="btn btn-dark btn-lg" href="index.php#accueil" role="button">Accueil</a>
+    <a class="btn btn-dark btn-lg" href="index.php#Apropos" role="button">A propos</a>
+    <a class="btn btn-dark btn-lg" href="index.php#competences" role="button">Compétences</a>
+    <a class="btn btn-dark btn-lg" href="index.php#experience" role="button">Expérience</a>
+    <a class="btn btn-dark btn-lg" href="index.php#accueil" role="button">Formation</a>
+    <a class="btn btn-dark btn-lg" href="index.php#accueil" role="button">Contact</a>
+
+
     </ol>
    </nav>
     <main>
