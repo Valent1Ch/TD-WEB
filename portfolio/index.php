d57a6bb9088ebbedbc0d0aa9764da1027c031f0a
Valent1Ch <valentin.choisnet@sts-sio-caen.info>
2022-12-22 14:53:04 +0100

    portfoliomaj

--- portfolio/index.php
+++ portfolio/index.php
@@ -28,7 +28,13 @@
    <main>
     <h1>BIENVENUE SUR MON PORTFOLIO</h1>
     <a name="accueil">Accueil</a>
+    <div class='elm'>
 
+   <p>Bonjour bienvenue sur le portfolio d'un etudiant en 1er année de BTS SLAM</p>
+
+
+
+</div>
     
     
 
@@ -53,19 +59,28 @@
   <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
 </svg></a>
       
-        
+      
 
     <?php
         $content=file_get_contents('competenceyaml.yaml');
         $yamlContent=yaml_parse($content);
         //echo '<pre>'.print_r($yamlContent,true).'</pre>';
-        $competences=$yamlContent['compétences'];
+        $competences=$yamlContent['compétences'][0];
+        $competences2=$yamlContent['compétences'][1];
+        $competences3=$yamlContent['compétences'][2];
+        $competences4=$yamlContent['compétences'][3];
+        $competences5=$yamlContent['compétences'][4];
+            echo "<ul class='elm'>";
+            echo "<li>$competences</li>";
+            echo "<li>$competences2</li>";
+            echo "<li>$competences3</li>";
+            echo "<li>$competences4</li>";
+            echo "<li>$competences5</li>";
+            echo '<img id="competenceimg" src="'.$yamlContent["imgcomp"]["img"].'" />';
+            echo "</ul>";
+      
 
-        echo "<ul class='elm'>";
-        foreach($competences as $comp){
-            echo "<li>$comp</li>";
-        }
-        echo "</ul>";
+      
 
     ?>
 
